eff803ff2990df61db343092add1fac11db40dfb
Casey Webb <notcaseywebb@gmail.com>
2019-01-11 15:52:58 -0600

    :construction: WIP

---
+++ 2018/12/12.ts
@@ -0,0 +1,102 @@
+/**
+ * Suppose we represent our file system by a string in the following manner:
+ * 
+ * The string "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext" represents:
+ * 
+ * dir
+ *     subdir1
+ *     subdir2
+ *         file.ext
+ * 
+ * The directory dir contains an empty sub-directory subdir1 and a sub-directory
+ * subdir2 containing a file file.ext.
+ * 
+ * The string "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" represents:
+ * 
+ * dir
+ *     subdir1
+ *         file1.ext
+ *         subsubdir1
+ *     subdir2
+ *         subsubdir2
+ *             file2.ext
+ * 
+ * The directory dir contains two sub-directories subdir1 and subdir2. subdir1
+ * contains a file file1.ext and an empty second-level sub-directory subsubdir1.
+ * subdir2 contains a second-level sub-directory subsubdir2 containing a file
+ * file2.ext.
+ * 
+ * We are interested in finding the longest (number of characters) absolute path
+ * to a file within our file system. For example, in the second example above,
+ * the longest absolute path is "dir/subdir2/subsubdir2/file2.ext", and its
+ * length is 32 (not including the double quotes).
+ * 
+ * Given a string representing the file system in the above format, return the
+ * length of the longest absolute path to a file in the abstracted file system.
+ * If there is no file in the system, return 0.
+ * 
+ * Note:
+ * 
+ * The name of a file contains at least a period and an extension.
+ * 
+ * The name of a directory or sub-directory will not contain a period.
+ */
+
+export {}
+
+function isFile(fd: string) {
+  return fd.includes('.')
+}
+
+class Directory {
+  public contents: (Directory | File)[] = []
+
+  constructor(public name: string) {}
+
+  public add(item: Directory | File) {
+    this.contents.push(item)
+  }
+
+  public toJS(): { [k: string]: any } {
+    return {
+      [this.name]: this.contents.map((item) => {
+        if (item instanceof File) {
+          return item.name
+        } else {
+          return item.toJS()
+        }
+      })
+    }
+  }
+
+  // public getLongestPathname(): string {
+  // }
+
+  public static fromString(str: string): Directory {
+    const [dirname, ...lines] = str.split('\n').map((line) => line.replace(/^\t/, ''))
+    const dir = new Directory(dirname)
+
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i]
+      if (isFile(line)) {
+        dir.add(new File(line))
+      } else {
+        const subdirLines = [line]
+        while (lines[i + 1] && lines[i + 1].startsWith('\t')) {
+          subdirLines.push(lines[++i])
+        }
+        dir.add(Directory.fromString(subdirLines.join('\n')))
+      }
+    }
+
+    return dir
+  }
+}
+
+class File {
+  constructor(public name: string) {}
+}
+
+export const dir = Directory.fromString('dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext')
+
+console.log(JSON.stringify(dir.toJS(), null, 2))
